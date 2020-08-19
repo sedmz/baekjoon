1fddb8c601ccd94a451c58d007956cb180bdda23
sedmz <48542327+sedmz@users.noreply.github.com>
2020-08-19 21:06:46 +0900

    배열, 시간초과 주의

--- [10816번] 숫자 카드 2/Main.java
+++ [10816번] 숫자 카드 2/Main.java
@@ -4,22 +4,20 @@ import java.io.InputStreamReader;
 
 class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static final int MAX_VALUE = 10000000;
+	static final int UNSIGNED = 10000000;
+	static int card[] = new int[UNSIGNED * 2 + 1];
 
 	public static void main(String[] args) throws IOException {
 		br.readLine();
-		String input[] = br.readLine().split(" ");
-		int numbers[] = new int[MAX_VALUE * 2 + 1];
 
-		for (int i = 0; i < input.length; i++)
-			numbers[Integer.parseInt(input[i]) + MAX_VALUE]++;
+		String inputs[] = br.readLine().split(" ");
+		for (String input : inputs)
+			card[Integer.parseInt(input) + UNSIGNED]++;
 
 		br.readLine();
-		String output[] = br.readLine().split(" ");
-		for (int i = 0; i < output.length; i++) {
-			System.out.print(numbers[Integer.parseInt(output[i]) + MAX_VALUE]);
-			if (i < output.length - 1)
-				System.out.print(" ");
-		}
+
+		String nums[] = br.readLine().split(" ");
+		for (String num : nums)
+			System.out.print(card[Integer.parseInt(num) + UNSIGNED] + " ");
 	}
 }
