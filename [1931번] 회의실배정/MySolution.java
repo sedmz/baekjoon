37469c9b9729eb6ca6e24be216c7e193ba44ceb3
sedmz <48542327+sedmz@users.noreply.github.com>
2020-09-02 10:55:04 +0900

    Update and rename Main.java to MySolution.java

--- [1931번] 회의실배정/MySolution.java
+++ [1931번] 회의실배정/MySolution.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 class Main {
 	static Scanner sc = new Scanner(System.in);
-	static int[][] timeList;
+	static int[][] times;
 
-	public static void initTimeList() {
+	public static void init() {
 		int N = sc.nextInt();
-		timeList = new int[N][2];
+		times = new int[N][2];
 		for (int i = 0; i < N; i++) {
-			timeList[i][0] = sc.nextInt();
-			timeList[i][1] = sc.nextInt();
+			times[i][0] = sc.nextInt();
+			times[i][1] = sc.nextInt();
 		}
 	}
 
-	public static void sortTimeList() {
-		Arrays.sort(timeList, new Comparator<int[]>() {
+	public static void sort() {
+		Arrays.sort(times, new Comparator<int[]>() {
 			public int compare(int[] o1, int[] o2) {
 				if (o1[1] == o2[1])
 					return ((Comparable) o1[0]).compareTo(o2[0]);
@@ -26,11 +26,14 @@ class Main {
 	}
 
 	public static void main(String[] args) {
-		initTimeList();
-		sortTimeList();
+		init();
+		sort();
 
+		for (int i = 0; i < times.length; i++)
+			System.out.println(Arrays.toString(times[i]));
+		
 		int cnt = 0, beforeEnd = 0;
-		for (int[] time : timeList) {
+		for (int[] time : times) {
 			if (beforeEnd <= time[0]) {
 				beforeEnd = time[1];
 				cnt++;
