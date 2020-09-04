ac4619ebc0a4f2feab07b993686a4b8880fe4cd6
sedmz <48542327+sedmz@users.noreply.github.com>
2020-09-04 14:02:22 +0900

    DP, 이차원 배열 -> 일차원 배열로 개선

--- [1946번] 신입 사원/MySolution.java
+++ [1946번] 신입 사원/MySolution.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 class Main {
@@ -11,22 +9,15 @@ class Main {
 		while (T-- > 0) {
 			int N = sc.nextInt(), cnt = 0;
 
-			int[][] scores = new int[N][2];
+			int[] scores = new int[N];
 			for (int i = 0; i < N; i++) {
-				scores[i][0] = sc.nextInt();
-				scores[i][1] = sc.nextInt();
+				scores[sc.nextInt() - 1] = sc.nextInt();
 			}
 
-			Arrays.sort(scores, new Comparator<int[]>() {
-				public int compare(int[] o1, int[] o2) {
-					return ((Comparable) o1[0]).compareTo(o2[0]);
-				}
-			});
-
 			int best = N;
-			for (int[] score : scores) {
-				if (score[1] <= best) {
-					best = score[1];
+			for (int score : scores) {
+				if (score <= best) {
+					best = score;
 					cnt++;
 				}
 			}
