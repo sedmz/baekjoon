6dd196088da1784ecca92e57f867e4ebd3cff149
sedmz <48542327+sedmz@users.noreply.github.com>
2020-10-22 16:26:00 +0900

    변수명 수정

--- [1753번] 최단경로/MySolution.java
+++ [1753번] 최단경로/MySolution.java
@@ -7,13 +7,13 @@ class Main {
 		int V = sc.nextInt(), E = sc.nextInt();
 		int start = sc.nextInt();
 
-		ArrayList<int[]>[] routes = new ArrayList[V + 1];
-		for (int i = 1; i < routes.length; i++)
-			routes[i] = new ArrayList<int[]>();
+		ArrayList<int[]>[] edges = new ArrayList[V + 1];
+		for (int i = 1; i < edges.length; i++)
+			edges[i] = new ArrayList<int[]>();
 
 		for (int i = 0; i < E; i++) {
-			int from = sc.nextInt();
-			routes[from].add(new int[] { from, sc.nextInt(), sc.nextInt() });
+			int[] edge = new int[] {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+			edges[edge[0]].add(edge);
 		}
 
 		Set<Integer> visited = new HashSet<>();
@@ -24,7 +24,7 @@ class Main {
 				return Integer.compare(o1[2], o2[2]);
 			}
 		});
-		remainEdges.addAll(routes[start]);
+		remainEdges.addAll(edges[start]);
 
 		int[] shortest = new int[V + 1];
 
@@ -34,7 +34,8 @@ class Main {
 			if (!visited.contains(edge[1])) {
 				visited.add(edge[1]);
 				shortest[edge[1]] = edge[2];
-				for (int[] e : routes[edge[1]]) {
+				
+				for (int[] e : edges[edge[1]]) {
 					remainEdges.add(new int[] { e[0], e[1], shortest[e[0]] + e[2] });
 				}
 			}
